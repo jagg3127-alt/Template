c140d32955d7ae29b8974c6a27612b5629fb0a59
Jagg3127-alt <78183671+jagg3127-alt@users.noreply.github.com>
2022-03-31 12:44:44 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask('Josh')
+# webcode = open('webcode.html').read() - not needed
 
 @app.route('/')
-def index():
-    return 'Hello world'
+def webprint():
+    return render_template('webcode.html') 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host = '0.0.0.0', port = 3000)
